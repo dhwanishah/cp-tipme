f19bcea49081c39f325545871cd59afdbf7b1cd0
Dhwani Shah <dshah202@gmail.com>
2017-03-04 12:52:41 -0500

    First working iteration of tip calculations with selections completed.

--- TipMe/ViewController.swift
+++ TipMe/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var totalAmountLabel: UILabel!
+    @IBOutlet weak var amountEnteredTextField: UITextField!
+    @IBOutlet weak var tipPercentageSelection: UISegmentedControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,21 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func onAmountChange(_ sender: AnyObject) {
+        let tipAmounts = [0.15, 0.18, 0.20]
+        
+        let amountEntered = Double(amountEnteredTextField.text!) ?? 0
+        let tip = amountEntered * tipAmounts[tipPercentageSelection.selectedSegmentIndex]
+        let total = amountEntered + tip
+        
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        totalAmountLabel.text = String(format: "$%.2f", total)
+    }
 
 }
 
