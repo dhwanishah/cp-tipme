e8ac98513fba9c9d177136d6a2c72f83f0b9000a
Dhwani Shah <dshah202@gmail.com>
2017-03-04 14:15:55 -0500

    Created Settings page and added dummy buttons. Created and attached a slider for tip value. Changed the default view color on start. Made the amount at first responder.

--- TipMe/ViewController.swift
+++ TipMe/ViewController.swift
@@ -14,11 +14,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var totalAmountLabel: UILabel!
     @IBOutlet weak var amountEnteredTextField: UITextField!
     @IBOutlet weak var tipPercentageSelection: UISegmentedControl!
+    @IBOutlet weak var tipSliderAmountLabel: UILabel!
+    @IBOutlet weak var tipAmountSlider: UISlider!
     
+    override func viewWillAppear(_ animated: Bool) {
+        self.view.backgroundColor = UIColor.init(red: 0, green: 233, blue: 0, alpha: 1)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        amountEnteredTextField.becomeFirstResponder()
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,12 +37,16 @@ class ViewController: UIViewController {
     }
     
     @IBAction func onAmountChange(_ sender: AnyObject) {
-        let tipAmounts = [0.15, 0.18, 0.20]
+        //let tipAmounts = [0.15, 0.18, 0.20]
+        tipAmountSlider.setValue(Float(lroundf(tipAmountSlider.value)), animated: true)
         
         let amountEntered = Double(amountEnteredTextField.text!) ?? 0
-        let tip = amountEntered * tipAmounts[tipPercentageSelection.selectedSegmentIndex]
+        let tipAmount = Double(tipAmountSlider.value / 100)
+        //let tip = amountEntered * tipAmounts[tipPercentageSelection.selectedSegmentIndex]
+        let tip = amountEntered * tipAmount
         let total = amountEntered + tip
         
+        tipSliderAmountLabel.text = String(format: "%.0f%%", tipAmountSlider.value)
         tipAmountLabel.text = String(format: "$%.2f", tip)
         totalAmountLabel.text = String(format: "$%.2f", total)
     }
